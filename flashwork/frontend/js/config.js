7683961dd8a4d6f7bcc941ca94602bed9080263d
frostzapper <69069239+frostzapper@users.noreply.github.com>
2026-02-21 08:39:59 +0530

    Changed api
    updated from local to onrender

--- flashwork/frontend/js/config.js
+++ flashwork/frontend/js/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'fetch("https://https://student-freelancer-1.onrender.com//api")';
 
 export const ENDPOINTS = {
     // Auth
